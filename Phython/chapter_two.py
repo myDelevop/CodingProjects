0e68686fee98e9590e32f46a279e5b180ac6c714
Rocco Caliandro <rockdesires@gmail.com>
2022-05-14 14:35:00 +0100

    py: implementation of even_odd method

---
+++ Phython/chapter_two.py
@@ -0,0 +1,23 @@
+import utilities
+
+
+# This method take in input an array and reorder so that the even entries
+# appears first and then the odd entries later
+def even_odd(arr):
+    even_index = 0
+    odd_index = len(arr)-1
+    while even_index < odd_index:
+        if array[even_index] % array[odd_index] == 0:
+            even_index = even_index + 1
+        else:
+            utilities.swap_elements(array, even_index, odd_index)
+            odd_index = odd_index - 1
+    return
+
+
+cars = ["Ford", "Volvo", "BMW"]
+print(utilities.array_to_string(cars))
+
+array = [9, 4, 1, 8, -3, 5]
+even_odd(array)
+print(utilities.array_to_string(array))
